58d410471d5718f4c4bcec52547ec2909111d3b1
blcklamb <whenucan35@gmail.com>
2023-11-15 15:25:22 +0900

    fix: sync force 옵션 중지

--- models/sync.js
+++ models/sync.js
@@ -1,11 +1,11 @@
 const { sequelize } = require('./index.js');
 const sync = () => {
 	sequelize
-		.sync({ force: true })
+		.sync({ force: false })
 		.then(() => {
 			console.log('데이터베이스 생성 완료');
 		})
-		.catch((error) => {
+		.catch(error => {
 			console.error(error);
 		});
 };
