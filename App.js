7b47552fee18da4c0f0f2f3adac085e93ab264eb
blcklamb <whenucan35@gmail.com>
2023-11-15 18:23:09 +0900

    feat: app에 router 추가

--- App.js
+++ App.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 const find_clothing_box_router = require('./routes/find_clothing_box_router.js');
+const my_page_user_info_edit_router = require('./routes/my_page/user_info_router.js');
+const my_page_post_list_router = require('./routes/my_page/post_list_router.js');
+const my_page_comment_list_router = require('./routes/my_page/comment_list_router.js');
 
 // middleware
 app.use(morgan('dev'));
@@ -18,5 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/find-clothing-box', find_clothing_box_router);
+app.use('/my-page/edit', my_page_user_info_edit_router);
+app.use('/my-page/post-list', my_page_post_list_router);
+app.use('/my-page/comment-list', my_page_comment_list_router);
 
 app.listen(port);
