fbb912332f9361d504a789c75e57e4c7b6a0c5c7
brandnewhoho@outlook.com <brandnewhoho@naver.com>
2023-11-17 16:08:41 +0900

    chore: 불필요한 부분 정리중

--- routes/nanum/nanum_list_comment_router.js
+++ routes/nanum/nanum_list_comment_router.js
@@ -7,7 +7,8 @@ router.post('/', async (req, res) => {
 	try {
 		// new_post.post_id = req.query.post_id;
 		const result = await Comment.create(new_comment);
-		res.send({ success: true, data: { comment_id: result.id }, message: '응답 글 등록 성공' });
+
+		res.send({ success: true, data: result, message: '응답 글 등록 성공' });
 	} catch (error) {
 		res.send({ success: false, data: '', message: error });
 	}
