1d230c7eb10df67046f6fe36765fcd4e21f3a5c5
brandnewhoho <brandnewhoho@outlook.com>
2023-11-19 03:54:49 +0900

    chore: pull로 생긴 conflict 해결과정

--- App.js
+++ App.js
@@ -15,15 +15,12 @@ const find_clothing_box_router = require('./routes/find_clothing_box_router.js')
 const my_page_user_info_edit_router = require('./routes/my_page/user_info_router.js');
 const my_page_post_list_router = require('./routes/my_page/post_list_router.js');
 const my_page_comment_list_router = require('./routes/my_page/comment_list_router.js');
-<<<<<<< HEAD
+const nanum_create_post_router = require('./routes/nanum_create_post_router.js');
+const nanum_post_router = require('./routes/nanum_post_router.js');
 const nanum_list_router = require('./routes/nanum/nanum_list_router.js');
 const nanum_post_detail_router = require('./routes/nanum/nanum_post_detail_router.js');
 const nanum_list_comment_router = require('./routes/nanum/nanum_list_comment_router.js');
 const auth_router = require('./routes/auth/auth_router.js');
-=======
-const nanum_create_post_router = require('./routes/nanum_create_post_router.js');
-const nanum_post_router = require('./routes/nanum_post_router.js');
->>>>>>> 84a2f1952e0dac953667291c8488db3ffa28920d
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -34,15 +31,12 @@ app.use('/find-clothing-box', find_clothing_box_router);
 app.use('/my-page/edit', my_page_user_info_edit_router);
 app.use('/my-page/post-list', my_page_post_list_router);
 app.use('/my-page/comment-list', my_page_comment_list_router);
-<<<<<<< HEAD
+app.use('/nanum-create-post', nanum_create_post_router);
+app.use('/nanum-post', nanum_post_router);
 app.use('/nanum/list', nanum_list_router);
 app.use('/nanum/post/detail', nanum_post_detail_router);
 app.use('/nanum/list/comment', nanum_list_comment_router);
 app.use('/auth', auth_router);
-=======
-app.use('/nanum-create-post', nanum_create_post_router);
-app.use('/nanum-post', nanum_post_router);
->>>>>>> 84a2f1952e0dac953667291c8488db3ffa28920d
 
 app.listen(port, () => {
 	console.log('Express server listening on port ' + port);
